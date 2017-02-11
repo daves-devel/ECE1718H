34c24ade22c3d5950f23b7479bb58a3dee81646e
ikhan1 <ikhan1@MK-LW10-IKHAN1>
2017-02-10 20:12:36 -0500

    Irfan - Updating codec engine. Syntax is not accurate yet. Will update as I get binaries up and running and submitted.

--- codec_engine.py
+++ codec_engine.py
@@ -1,6 +1,6 @@
 #Author: Irfan Khan (khanirf1) 
 #Student Number: 999207665
-#Description: This script Upsamples a YUV420 Video Sequence to YUV444
+#Description: The Codec Engine for Assignment 1 
 
 import os
 import sys
@@ -9,26 +9,16 @@ def main(argv):
 
 	# Global Variables
 	# ------------------------------
+	testname	= ""
 	infile 		= ""
 	outfile		= ""
-	width		= 0
-	height 		= 0
-	frames		= 0
-	searchrange	= 0
-	rounding	= 0
-
--width 352 
--height 288 
--curfile 'C:\Users\ikhan1\Documents\ECE1718\foreman_cif_Y_352x288.yuv' 
--recfile 'C:\Users\ikhan1\Documents\ECE1718\reconstructed.yuv' 
--mvfile '\C:\Users\ikhan1\Documents\ECE1718\mvfile.txt'
--resfile 'C:\Users\ikhan1\Documents\ECE1718\residual.yuv'
--frames 100 
--block 2 
--range 1 
--round 2 
--gmvx 'C:\Users\ikhan1\Documents\ECE1718\GMVX' 
--gmvy 'C:\Users\ikhan1\Documents\ECE1718\GMVY'
+	int width		= 0
+	int height 		= 0
+	int frames		= 0
+	int searchrange	= 0
+	int rounding	= 0
+	int with_old	= 0
+	int height_old  = 0
 
 	# Parse Input Arguments 
 	# ------------------------------
@@ -55,5 +45,35 @@ def main(argv):
 		if (argv[index] == "-round"):
 			rounding = int(argv[index+1])
 
-	# YUV Pre-processing
+	# YUV Pre-Processing
 	# -------------------------------
+
+	# LumaExtractor
+	LumaExtractor/LumaExtractor.exe -frames frames -filein infile -width width -height height -outfile testname_LumaExtracted_frames@widthxheight.yuv 
+
+
+	height_old	= height
+	width_old	= width
+
+	if (( width% block) > 0):
+		width = (width/block)*block + 1;
+
+	if ((height%block) > 0);
+		height = (height/block)*block + 1;
+
+	# LumaPadder
+	LumaPad/LumaPad.exe -frames frames -width width_old -height height_old -block block -filein testname_LumaExtracted_frames@widthxheight.yuv -fileout testname_LumaPadded_frames@widthxheight.yuv
+
+	# Encoder
+	# -------------------------------
+	Encoder/Encoder.exe -frames frames -width width -height height -block block -range searchrange -round rounding -curfile testname_LumaPadded_frames@widthxheight.yuv -recfile testname_Reconstructed_frames@widthxheight.yuv -resfile testname_Residual_frames@widthxheight.yuv -mvfile testname_mvfile.txt -gmvx GMVX -gmvy GMVY
+
+	# Decoder
+	# -------------------------------
+	Decoder/Decoder.exe -frames frames -width width -height height -block block 
+
+	# Verification
+	# -------------------------------
+
+	# Analysis
+	# --------------------------------
