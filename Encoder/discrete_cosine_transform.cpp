ec523c9a61f9e7849c5563997d9880dcb9c51add
david.chakkuthara <david.chakkuthara@gmail.com>
2017-03-01 19:28:36 -0500

    Cleanning up source file

--- Encoder/discrete_cosine_transform.cpp
+++ Encoder/discrete_cosine_transform.cpp
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-
-void dct(float **DCTMatrix, float **Matrix, int N, int M);
-void write_mat(FILE *fp, float **testRes, int N, int M);
-void idct(float **Matrix, float **DCTMatrix, int N, int M);
-float **calloc_mat(int dimX, int dimY);
-void free_mat(float **p);
-
+#include <discrete_cosine_transform.h>
 
 float **calloc_mat(int dimX, int dimY) {
 	float **m = calloc(dimX, sizeof(float*));
