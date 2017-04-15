7e594caea4bf6dc86c84177ee9a8832314707a82
Juan <Juan@DESKTOP-F7SV748>
2017-04-15 00:17:50 -0400

    reverting

--- Encoder/DiffDec.h
+++ Encoder/DiffDec.h
@@ -5,23 +5,50 @@ void diff_dec_wrapper(MDIFF** MDIFF_VECTOR, MDIFF** MDIFF_VECTOR_DIFF, int Frame
 void diff_dec(MDIFF** MDIFF_VECTOR, MDIFF** MDIFF_VECTOR_DIFF, int FrameType, int row, int col) {
 	if (FrameType == IFRAME) {
 		if (col == 0) {
+			MDIFF_VECTOR[row][col].split = MDIFF_VECTOR_DIFF[row][col].split;
 			MDIFF_VECTOR[row][col].MODE = MDIFF_VECTOR_DIFF[row][col].MODE;
+			MDIFF_VECTOR[row][col].MODE2 = MDIFF_VECTOR_DIFF[row][col].MODE2;
+			MDIFF_VECTOR[row][col].MODE3 = MDIFF_VECTOR_DIFF[row][col].MODE3;
+			MDIFF_VECTOR[row][col].MODE4 = MDIFF_VECTOR_DIFF[row][col].MODE4;
 		}
 		else {
+			MDIFF_VECTOR[row][col].split = MDIFF_VECTOR_DIFF[row][col].split + MDIFF_VECTOR[row][col - 1].split;
 			MDIFF_VECTOR[row][col].MODE = MDIFF_VECTOR_DIFF[row][col].MODE + MDIFF_VECTOR[row][col - 1].MODE;
+			MDIFF_VECTOR[row][col].MODE2 = MDIFF_VECTOR_DIFF[row][col].MODE2 + MDIFF_VECTOR[row][col - 1].MODE2;
+			MDIFF_VECTOR[row][col].MODE3 = MDIFF_VECTOR_DIFF[row][col].MODE3 + MDIFF_VECTOR[row][col - 1].MODE3;
+			MDIFF_VECTOR[row][col].MODE4 = MDIFF_VECTOR_DIFF[row][col].MODE4 + MDIFF_VECTOR[row][col - 1].MODE4;
 		}
 	}
 	else {//P frame
 		if (col == 0) {
+			MDIFF_VECTOR[row][col].split = MDIFF_VECTOR_DIFF[row][col].split;
 			MDIFF_VECTOR[row][col].X = MDIFF_VECTOR_DIFF[row][col].X;
+			MDIFF_VECTOR[row][col].X2 = MDIFF_VECTOR_DIFF[row][col].X2;
+			MDIFF_VECTOR[row][col].X3 = MDIFF_VECTOR_DIFF[row][col].X3;
+			MDIFF_VECTOR[row][col].X4 = MDIFF_VECTOR_DIFF[row][col].X4;
 			MDIFF_VECTOR[row][col].Y = MDIFF_VECTOR_DIFF[row][col].Y;
+			MDIFF_VECTOR[row][col].Y2 = MDIFF_VECTOR_DIFF[row][col].Y2;
+			MDIFF_VECTOR[row][col].Y3 = MDIFF_VECTOR_DIFF[row][col].Y3;
+			MDIFF_VECTOR[row][col].Y4 = MDIFF_VECTOR_DIFF[row][col].Y4;
 			MDIFF_VECTOR[row][col].ref = MDIFF_VECTOR_DIFF[row][col].ref;
+			MDIFF_VECTOR[row][col].ref2 = MDIFF_VECTOR_DIFF[row][col].ref2;
+			MDIFF_VECTOR[row][col].ref3 = MDIFF_VECTOR_DIFF[row][col].ref3;
+			MDIFF_VECTOR[row][col].ref4 = MDIFF_VECTOR_DIFF[row][col].ref4;
 		}
 		else {
+			MDIFF_VECTOR[row][col].split = MDIFF_VECTOR_DIFF[row][col].split + MDIFF_VECTOR[row][col - 1].split;
 			MDIFF_VECTOR[row][col].X = MDIFF_VECTOR_DIFF[row][col].X + MDIFF_VECTOR[row][col - 1].X;
+			MDIFF_VECTOR[row][col].X2 = MDIFF_VECTOR_DIFF[row][col].X2 + MDIFF_VECTOR[row][col - 1].X2;
+			MDIFF_VECTOR[row][col].X3 = MDIFF_VECTOR_DIFF[row][col].X3 + MDIFF_VECTOR[row][col - 1].X3;
+			MDIFF_VECTOR[row][col].X4 = MDIFF_VECTOR_DIFF[row][col].X4 + MDIFF_VECTOR[row][col - 1].X4;
 			MDIFF_VECTOR[row][col].Y = MDIFF_VECTOR_DIFF[row][col].Y + MDIFF_VECTOR[row][col - 1].Y;
+			MDIFF_VECTOR[row][col].Y2 = MDIFF_VECTOR_DIFF[row][col].Y2 + MDIFF_VECTOR[row][col - 1].Y2;
+			MDIFF_VECTOR[row][col].Y3 = MDIFF_VECTOR_DIFF[row][col].Y3 + MDIFF_VECTOR[row][col - 1].Y3;
+			MDIFF_VECTOR[row][col].Y4 = MDIFF_VECTOR_DIFF[row][col].Y4 + MDIFF_VECTOR[row][col - 1].Y4;
 			MDIFF_VECTOR[row][col].ref = MDIFF_VECTOR_DIFF[row][col].ref + MDIFF_VECTOR[row][col - 1].ref;
-
+			MDIFF_VECTOR[row][col].ref2 = MDIFF_VECTOR_DIFF[row][col].ref2 + MDIFF_VECTOR[row][col - 1].ref2;
+			MDIFF_VECTOR[row][col].ref3 = MDIFF_VECTOR_DIFF[row][col].ref3 + MDIFF_VECTOR[row][col - 1].ref3;
+			MDIFF_VECTOR[row][col].ref4 = MDIFF_VECTOR_DIFF[row][col].ref4 + MDIFF_VECTOR[row][col - 1].ref4;
 		}
 	}
 }
@@ -29,25 +56,37 @@ void diff_dec(MDIFF** MDIFF_VECTOR, MDIFF** MDIFF_VECTOR_DIFF, int FrameType, in
 void diff_dec_wrapper(MDIFF** MDIFF_VECTOR, MDIFF** MDIFF_VECTOR_DIFF, int FrameType, int height, int width, int block, int frame) {
 #ifdef TRACE_ON
 	FILE* file_vector_org;
-	FILE* file_vector_aft;
+	//FILE* file_vector_aft;
 	char buf[0x100];
 	snprintf(buf, sizeof(buf), "testdata\\MDIFF_ORG_DEC%d.txt", frame);
 	file_vector_org = fopen(buf, "w");
-	snprintf(buf, sizeof(buf), "testdata\\MDIFF_AFT_DEC%d.txt", frame);
-	file_vector_aft = fopen(buf, "w");
+	//snprintf(buf, sizeof(buf), "testdata\\MDIFF_AFT_DEC%d.txt", frame);
+	//file_vector_aft = fopen(buf, "w");
 #endif
 	for (int row = 0; row < height; row = row + block) {
 		for (int col = 0; col < width; col = col + block) {
 			diff_dec(MDIFF_VECTOR, MDIFF_VECTOR_DIFF, FrameType, row / block, col / block);
 #ifdef TRACE_ON
+			//fprintf(file_vector_org, "Block Y=%d X=%d\n", row / block, col / block);
+			//fprintf(file_vector_org, "FRAME_TYPE: %d MODE: %d X: %d Y: %d ref %d\n", FrameType, MDIFF_VECTOR[row / block][col / block].MODE, MDIFF_VECTOR[row / block][col / block].X, MDIFF_VECTOR[row / block][col / block].Y, MDIFF_VECTOR[row / block][col / block].ref);
+			//fprintf(file_vector_aft, "Block Y=%d X=%d\n", row / block, col / block);
+			//fprintf(file_vector_aft, "FRAME_TYPE: %d MODE: %d X: %d Y: %d  ref %d\n", FrameType, MDIFF_VECTOR_DIFF[row / block][col / block].MODE, MDIFF_VECTOR_DIFF[row / block][col / block].X, MDIFF_VECTOR_DIFF[row / block][col / block].Y, MDIFF_VECTOR_DIFF[row / block][col / block].ref);
 			fprintf(file_vector_org, "Block Y=%d X=%d\n", row / block, col / block);
-			fprintf(file_vector_org, "FRAME_TYPE: %d MODE: %d X: %d Y: %d ref %d\n", FrameType, MDIFF_VECTOR[row / block][col / block].MODE, MDIFF_VECTOR[row / block][col / block].X, MDIFF_VECTOR[row / block][col / block].Y, MDIFF_VECTOR[row / block][col / block].ref);
-			fprintf(file_vector_aft, "Block Y=%d X=%d\n", row / block, col / block);
-			fprintf(file_vector_aft, "FRAME_TYPE: %d MODE: %d X: %d Y: %d  ref %d\n", FrameType, MDIFF_VECTOR_DIFF[row / block][col / block].MODE, MDIFF_VECTOR_DIFF[row / block][col / block].X, MDIFF_VECTOR_DIFF[row / block][col / block].Y, MDIFF_VECTOR_DIFF[row / block][col / block].ref);
+			fprintf(file_vector_org, "FRAME_TYPE: %d SPLIT %d MODE: %d %d %d %d X: %d %d %d %d Y: %d %d %d %d ref %d %d %d %d\n", FrameType, MDIFF_VECTOR[row / block][col / block].split,
+				MDIFF_VECTOR[row / block][col / block].MODE, MDIFF_VECTOR[row / block][col / block].MODE2, MDIFF_VECTOR[row / block][col / block].MODE3, MDIFF_VECTOR[row / block][col / block].MODE4,
+				MDIFF_VECTOR[row / block][col / block].X, MDIFF_VECTOR[row / block][col / block].X2, MDIFF_VECTOR[row / block][col / block].X3, MDIFF_VECTOR[row / block][col / block].X4,
+				MDIFF_VECTOR[row / block][col / block].Y, MDIFF_VECTOR[row / block][col / block].Y2, MDIFF_VECTOR[row / block][col / block].Y3, MDIFF_VECTOR[row / block][col / block].Y4,
+				MDIFF_VECTOR[row / block][col / block].ref, MDIFF_VECTOR[row / block][col / block].ref2, MDIFF_VECTOR[row / block][col / block].ref3, MDIFF_VECTOR[row / block][col / block].ref4);
+			/*fprintf(file_vector_aft, "Block Y=%d X=%d\n", row / block, col / block);
+			fprintf(file_vector_aft, "FRAME_TYPE: %d SPLIT %d MODE: %d %d %d %d X: %d %d %d %d Y: %d %d %d %d ref %d %d %d %d\n", FrameType, MDIFF_VECTOR_DIFF[row / block][col / block].split,
+			MDIFF_VECTOR_DIFF[row / block][col / block].MODE, MDIFF_VECTOR_DIFF[row / block][col / block].MODE2, MDIFF_VECTOR_DIFF[row / block][col / block].MODE3, MDIFF_VECTOR_DIFF[row / block][col / block].MODE4,
+			MDIFF_VECTOR_DIFF[row / block][col / block].X, MDIFF_VECTOR_DIFF[row / block][col / block].X2, MDIFF_VECTOR_DIFF[row / block][col / block].X3, MDIFF_VECTOR_DIFF[row / block][col / block].X4,
+			MDIFF_VECTOR_DIFF[row / block][col / block].Y, MDIFF_VECTOR_DIFF[row / block][col / block].Y2, MDIFF_VECTOR_DIFF[row / block][col / block].Y3, MDIFF_VECTOR_DIFF[row / block][col / block].Y4,
+			MDIFF_VECTOR_DIFF[row / block][col / block].ref, MDIFF_VECTOR_DIFF[row / block][col / block].ref2, MDIFF_VECTOR_DIFF[row / block][col / block].ref3, MDIFF_VECTOR_DIFF[row / block][col / block].ref4);*/
 #endif // TRACE_ON
 
 		}
 	}
 	fclose(file_vector_org);
-	fclose(file_vector_aft);
+	//fclose(file_vector_aft);
 }
